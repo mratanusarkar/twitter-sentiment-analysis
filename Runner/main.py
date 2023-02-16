4e0f003eb5dd5afee7c01fabf2b755c8de12c855
Atanu Sarkar <mratanusarkar@gmail.com>
2023-02-16 21:03:47 +0530

    update main with word cloud generator

--- Runner/main.py
+++ Runner/main.py
@@ -1,13 +1,16 @@
 from module.scraper import TweetScraper
+from module.generator import TwitterWordCloud
 
 # create helper objects
-tweetScraper = TweetScraper()
+tweet_scraper = TweetScraper()
+tweet_wc = TwitterWordCloud()
 
 # set parameters
 topic_title = 'ISRO'
 query = '@isro'
 limit = 10
+exclude_words = []
 
 # scrape tweets
-rawData = tweetScraper.get_tweets(query, limit)
-print(rawData)
+rawData = tweet_scraper.get_tweets(query, limit)
+tweet_wc.generate_word_cloud_v2(rawData, topic_title, exclude_words, 1080, 720)
